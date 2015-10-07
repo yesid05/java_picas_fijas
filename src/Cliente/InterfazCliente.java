f2b2b703eee0f5f1caa968a7ca231932d13beffb
Yesid Caicedo <caicedopacho@hotmail.com>
2015-10-07 17:25:18 -0500

    Picas fijas ayuda en el juego

--- src/Cliente/InterfazCliente.java
+++ src/Cliente/InterfazCliente.java
@@ -12,12 +12,9 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JButton;
@@ -80,8 +77,8 @@ public class InterfazCliente extends JFrame implements ActionListener, KeyListen
         lblExplicacion = new JLabel();
         lblExplicacion.setFont(new Font("Arial", 1, 18));
         lblExplicacion.setForeground(new Color(204, 0, 0));
-        lblExplicacion.setText("<html><p>El servidor genera tres números aleatorios,</p>"
-                + "<p>escriba tres números, si alguno de los números</p>"
+        lblExplicacion.setText("<html><p>El servidor genera cuatro números aleatorios,</p>"
+                + "<p>escriba cuatro números, si alguno de los números</p>"
                 + "<p>está en la posición en que se encuentra en el servidor</p>"
                 + "<p>se mostrara como fija, si el numero está en la secuencia</p>"
                 + "<p>paro no en la posición correcta se mostrara como pica.</p></html>");
